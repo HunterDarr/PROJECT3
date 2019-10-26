89ddaf5fe831c765e6975273611e5d19ce9ed7d1
HunterDarr <Hunter.darr.one@gmail.com>
2019-10-25 23:13:46 -0500

    Added constructor

--- Dropbox/School Documents/Computer Programming/Saved eclipse programs/Eclipse Programs/Project 3/src/MesoAsciiCal.java
+++ Dropbox/School Documents/Computer Programming/Saved eclipse programs/Eclipse Programs/Project 3/src/MesoAsciiCal.java
@@ -1,5 +1,16 @@
 
 public class MesoAsciiCal extends MesoAsciiAbstract 
 {
-   
+	private String stid;
+	
+	
+	public MesoAsciiCal(String stid)   {
+		this.stid = stid;
+		
+	}
+	
+	
+	public int calAverage()   {
+//		char[] stidChars = 
+	}
 }
