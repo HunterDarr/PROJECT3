0511dafbb2486fe3682cce7d5a82fbc48ff81f3b
HunterDarr <Hunter.darr.one@gmail.com>
2019-10-25 20:48:49 -0500

    Added testing class

---
+++ Dropbox/School Documents/Computer Programming/Saved eclipse programs/Eclipse Programs/Project 3/src/DateTimeOneTest.java
@@ -0,0 +1,79 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeOneTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void getValueOfSecondTest() {
+		DateTimeOne dateTimeOne = new DateTimeOne();
+
+		int actual = dateTimeOne.getValueOfSecond();
+		int expected = (int)(System.currentTimeMillis() / 1000 % 60);
+		assertEquals(expected, actual);
+	}
+	
+	
+	@Test
+	public void testDateTimeOtherCity()    {
+		DateTimeOne test = new DateTimeOne();
+		System.out.println("testDateTimeOtherCity");
+		test.dateTimeOfOtherCity();	
+	}
+	
+	
+	@Test
+	public void testDateTimeNow()   {
+		DateTimeOne test = new DateTimeOne();
+		System.out.println("testDateTimeNow");
+		test.dateTimeNow();
+	}
+	
+	
+	@Test
+	public void testDateTimeDifferentZone()   {
+		DateTimeOne test = new DateTimeOne();
+		System.out.println("testDateTimeDifferentZone");
+		test.dateTimeDifferentZone();
+	}
+	
+	
+	@Test
+	public void testTimeZoneHashMap()   {
+		DateTimeOne test = new DateTimeOne();
+		System.out.println("testTimeZoneHashMap()");
+		test.timeZoneHashMap();
+	}
+	
+	@Test
+	public void testDaysOfCurrentMonth()   {
+		DateTimeTwo test = new DateTimeTwo();
+		System.out.println("testDaysOfCurrentMonth");
+		test.daysOfCurrentMonth();
+	}
+	
+	@Test
+	public void testDaysOfAnyMonth()   {
+		DateTimeTwo test = new DateTimeTwo();
+		System.out.println("testDaysOfAnyMonth");
+		test.daysOfAnyMonth(11, 2019);
+		test.daysOfAnyMonth(2, 2020);
+		
+	}
+	
+	@Test
+	public void testCompareYear() throws IOException   {
+		System.out.println("testCompareYear");
+		DateTimeTwo test = new DateTimeTwo();
+		test.compareYear();
+
+	}
+
+}
