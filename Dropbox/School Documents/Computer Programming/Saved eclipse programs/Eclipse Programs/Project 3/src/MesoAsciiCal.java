30498a5ad9b774c0a6123e493d35ba82a1d64640
HunterDarr <Hunter.darr.one@gmail.com>
2019-10-26 01:12:02 -0500

    worked on calAverage()

--- Dropbox/School Documents/Computer Programming/Saved eclipse programs/Eclipse Programs/Project 3/src/MesoAsciiCal.java
+++ Dropbox/School Documents/Computer Programming/Saved eclipse programs/Eclipse Programs/Project 3/src/MesoAsciiCal.java
@@ -4,13 +4,26 @@ public class MesoAsciiCal extends MesoAsciiAbstract
 	private String stid;
 	
 	
-	public MesoAsciiCal(String stid)   {
-		this.stid = stid;
+	
+	public MesoAsciiCal(MesoStation mesoStation)   {
+		this.stid = mesoStation.getStID();
 		
 	}
 	
 	
 	public int calAverage()   {
-//		char[] stidChars = 
+		char[] stidChars = new char[4];
+		for (int i = 0; i < stidChars.length; i++)   {
+			stidChars[i] = stid.charAt(i);
+		}
+		double doubleAverage = (stidChars[0] + stidChars[1] + stidChars[2] + stidChars[3]) / 4.0;
+		int asciiCeil = (int)(Math.ceil(doubleAverage));
+		
+		int asciiFloor = (int)(Math.floor(doubleAverage));
+		
+		int asciiRound = (int)(Math.round((asciiCeil + asciiFloor)/2.0));
+		
+		return asciiRound;
+		
 	}
 }
